a80dad064c0319cc87e2dbf3994ba40e748f75b2
Lihi Micner <lihi.micner@post.runi.ac.il>
2025-11-07 19:20:19 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,27 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		String mode = args[1];
+		if (mode.equals("v")) {
+			for (int seed = 1; seed <= n; seed++) {
+				int count = 0;
+				int current = seed;
+				do {
+                    System.out.print(current + " ");
+                    if (current % 2 == 0) {
+                        current = current / 2;
+                    } else {
+                        current = current * 3 + 1;
+                    }
+                    count++;
+                } while (current != 1);
+
+                System.out.println("1 (" + (count + 1) + ")");
+			}
+			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		} else if (mode.equals("c")) {
+			System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		}
 	}
 }
