d4a107dab74cd4b78e0dfc3d4584567679093bc6
Lihi Micner <lihi.micner@post.runi.ac.il>
2025-11-07 18:44:27 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -18,7 +18,7 @@ public class Cheers {
                         || letter == 'M' || letter == 'N' || letter == 'O' || letter == 'R' || letter == 'S' || letter == 'X') {
                           System.out.println("Give me an " + letter + ": " + letter + "!");      
                         } else {                         
-                                System.out.println("Give me a " + letter + ": " + letter + "!");
+                                System.out.println("Give me a  " + letter + ": " + letter + "!");
                         }
                 } 
                 System.out.println("What does that spell?");
