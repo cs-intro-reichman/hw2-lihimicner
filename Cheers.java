67d8e4a284a5a5b6fd85b46d24b9ce383d360333
Lihi Micner <lihi.micner@post.runi.ac.il>
2025-11-07 18:40:50 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,22 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String name = args[0];
+                name = name.toUpperCase();
+                int rounds = Integer.parseInt(args[1]);
+                int nameLength = name.length();
+                for (int i = 0; i < nameLength; i++) {
+                        char letter = name.charAt(i);
+                        if (letter == 'A' || letter == 'E' || letter == 'F' || letter == 'H' || letter == 'I' || letter == 'L' 
+                        || letter == 'M' || letter == 'N' || letter == 'O' || letter == 'R' || letter == 'S' || letter == 'X') {
+                          System.out.println("Give me an " + letter + ": " + letter + "!");      
+                        } else {                         
+                                System.out.println("Give me a " + letter + ": " + letter + "!");
+                        }
+                } 
+                System.out.println("What does that spell?");
+                for (int count = 0; count < rounds; count++) {
+                     System.out.println(name + "!!!");   
+                }
         }
 }
